5dd5ed575631e8151281ad1299af245edd031198
Морозов Иван Александрович <ivan.morozov@tele2.ru>
2019-11-14 16:19:02 +0600

    Add barbers_migrations

--- db/migrate/20191114070358_create_clients.rb
+++ db/migrate/20191114070358_create_clients.rb
@@ -1,5 +1,6 @@
-class CreateClients < ActiveRecord::Migration[6.0]
+class CreateClients < ActiveRecord::Migration[4.2]
   def change
+  	
   	create_table :client do |t|
   		t.text :name
   		t.text :phone
